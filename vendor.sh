5a68010afd7dab9b5d477ff1715a4c9b9174d3fe
vz3 <victor.zapanta@gmail.com>
2014-04-10 14:54:05 -0400

    Adding vendor copy script. Edit to contact me copy. Updated foundation.js

---
+++ vendor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp ./bower_components/foundation/js/foundation.min.js ./js/vendor/
+cp ./bower_components/jquery/dist/jquery.min.js ./js/vendor/
+cp ./bower_components/modernizr/modernizr.js ./js/vendor/
